c557a3e776ca969ca06e84002547c5c465d306e7
Henry <soujiroboi@Waou.(none)>
2013-03-18 23:00:19 -0400

    Added triangles

--- src/Binaries/shaders/vShaderOrgAndDir.glsl
+++ src/Binaries/shaders/vShaderOrgAndDir.glsl
@@ -3,13 +3,28 @@
 
 in vec2 vRayPosition;
 
-uniform vec3 uCameraPosition;
-
 out vec3 org, dir;
 
 void main()
 {
-    gl_Position = vec4(vRayPosition, -1.0, 1.0);
-    org = uCameraPosition;
-    dir = normalize(vec3(vRayPosition.x, vRayPosition.y, -1.0));
+    
+    org = vec3(0, 0, 0);
+    
+    float width = 512;
+    float height = 512;
+    
+    float invWidth = 1 / width;
+    float invHeight = 1 / height;
+    float fov = 30;
+    float aspectratio = width / height;
+    float angle = tan(45 * fov / 180);
+    
+    float x = vRayPosition.x * 256 + 256;
+    float y = vRayPosition.y * 256 + 256;
+    
+    float xx = (2 * ((x + 0.5) * invWidth) - 1) * angle * aspectratio;
+    float yy = (1 - 2 * ((y + 0.5) * invHeight)) * angle;
+    
+    dir = normalize(vec3(xx, yy, -1.0));
+    gl_Position = vec4(xx, yy, -1.0, 1.0);
 }
