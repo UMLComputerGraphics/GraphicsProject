9070aa7aebbc6f599f68eb66e7e175c87f9d4c0d
Nicholas Ver Voort <moldorma@gmail.com>
2013-03-20 18:59:38 -0400

    changes to geometry shader
    still can't figure out how to pass data into this shader

--- src/Binaries/shaders/gParticle.glsl
+++ src/Binaries/shaders/gParticle.glsl
@@ -18,33 +18,36 @@ vec2 TexCoord;        // Texture coordinates
 void main () 
 {
 	// vec3 Pos = gl_in[0].gl_Position.xyz;
-	vec3 Pos = gl_Position.xyz;
+	int i;
+	for (i = 0; i < gl_VerticesIn; i++) { 
+		vec3 Pos = gl_PositionIn[i].xyz;
 
-	vec3 toCamera = normalize( gCamPos - Pos );
-	vec3 up = vec3( 0.0, 1.0, 0.0 );
-	vec3 right = cross( toCamera, up );
+		vec3 toCamera = normalize( gCamPos - Pos );
+		vec3 up = vec3( 0.0, 1.0, 0.0 );
+		vec3 right = cross( toCamera, up );
 
-	// 'Grow' quad around a point
-	Pos -= ( right * 0.5 );
-	gl_Position = gVP * vec4( Pos, 1.0 );
-	TexCoord = vec2( 0.0, 0.0);
-	EmitVertex();
+		// 'Grow' quad around a point
+		Pos -= ( right * 0.5 );
+		gl_Position = gVP * vec4( Pos, 1.0 );
+		TexCoord = vec2( 0.0, 0.0);
+		EmitVertex();
 
-	Pos.y += 1.0;
-	gl_Position = gVP * vec4( Pos, 1.0 );
-	TexCoord = vec2( 0.0, 1.0 );
-	EmitVertex();
+		Pos.y += 1.0;
+		gl_Position = gVP * vec4( Pos, 1.0 );
+		TexCoord = vec2( 0.0, 1.0 );
+		EmitVertex();
 
-	Pos.y -= 1.0;
-	Pos += right;
-	gl_Position = gVP * vec4( Pos, 1.0 );
-	TexCoord = vec2( 1.0, 0.0 );
-	EmitVertex();
+		Pos.y -= 1.0;
+		Pos += right;
+		gl_Position = gVP * vec4( Pos, 1.0 );
+		TexCoord = vec2( 1.0, 0.0 );
+		EmitVertex();
 
-	Pos.y += 1.0;
-	gl_Position = gVP * vec4( Pos, 1.0 );
-	TexCoord = vec2( 1.0, 1.0 );
-	EmitVertex();
-
-	EndPrimitive();
+		Pos.y += 1.0;
+		gl_Position = gVP * vec4( Pos, 1.0 );
+		TexCoord = vec2( 1.0, 1.0 );
+		EmitVertex();
+		
+		EndPrimitive();
+	}
 }
