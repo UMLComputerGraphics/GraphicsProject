971be08fa0352c3e581f6ed259619fed348e57e5
nuclearmistake@gmail.com <emccann@cs.uml.edu>
2013-03-24 11:25:16 -0400

    Allow shader file path resolution from ANY WORKING DIRECTORY

--- src/Common/InitShader.cpp
+++ src/Common/InitShader.cpp
@@ -7,7 +7,11 @@
  **/
 
 #include <cstdio>
+#include <string.h>
+#include <string>
+#include <sstream>
 #include <iostream>
+#include <vector>
 #include "platform.h"
 #include "InitShader.hpp"
 
@@ -33,8 +37,49 @@
 #define GL_GEOMETRY_SHADER 0x8DD9
 #endif
 
+// http://stackoverflow.com/questions/236129/splitting-a-string-in-c
+std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
+    std::stringstream ss(s);
+    std::string item;
+    while(std::getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+    return elems;
+}
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    return split(s, delim, elems);
+}
+
 namespace Angel {
   
+  /**
+     * InitInitShader is a preparation step allowing executables to be invoked from
+     * working directories OTHER than the one containing the shaders directory
+     *
+     * @param binloc argv[0] from main
+     *
+     * @return The relative path from the working directory to the directory containing shaders folder
+     */
+  const char* InitInitShader(const char* binloc = "")
+  {
+	  static std::string dondeestalosshaders;
+	  if (binloc == NULL || strlen(binloc)<=0) {
+		  return dondeestalosshaders.c_str();
+	  }
+	  std::vector<std::string> slapchop = split(std::string(binloc),'/');
+
+	  //slapchop[0..length-2] == path without executable name
+	  if (slapchop.size() > 1)
+	  {
+		  std::stringstream cat;
+		  for (int i=0;i<slapchop.size()-1;i++)
+			  cat << slapchop[i] << "/";
+		  dondeestalosshaders = cat.str();
+	  }
+  }
+
+
   /**
    * Read in a shader file into a NULL-terminated string.
    *
@@ -43,7 +88,9 @@ namespace Angel {
    * @return A pointer to the NULL terminated string.
    */
   static char *readShaderSource( const char* shaderFile ) {
-    FILE* fp = fopen( shaderFile, "r" );
+	std::stringstream wholepath;
+	wholepath << InitInitShader() << shaderFile;
+    FILE* fp = fopen( wholepath.str().c_str(), "r" );
     
     if ( fp == NULL ) {
       return NULL;
@@ -60,7 +107,7 @@ namespace Angel {
     
     return buf;
   }
-  
+
   /**
    * InitShader takes two shader sourcefiles and compiles them into a
    * shader program.
