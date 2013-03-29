44d849b96d5f483f7b0db8af5f9d3b8e8a8541d4
nuclearmistake@gmail.com <emccann@cs.uml.edu>
2013-03-29 08:59:21 -0400

    Make geometry shader handling cleaner (single InitShader function with default argument)

--- src/include/InitShader.hpp
+++ src/include/InitShader.hpp
@@ -21,28 +21,16 @@ namespace Angel {
   const char* InitInitShader( const char* binloc );
   
   /**
-   * InitShader takes two shader sourcefiles and compiles them into a
+   * InitShader takes two or three shader sourcefiles and compiles them into a
    * shader program.
    *
    * @param vShaderFile the vertex shader source file
    * @param fShaderFile the fragment shader source file
+   * @param gShaderFile the geometry shader source file (omit if not applicable)
    *
    * @return A handle to the compiled glsl program.
    */
-  GLuint InitShader( const char* vShaderFile, const char* fShaderFile );
-  
-  /**
-   * InitShader takes three shader sourcefiles and compiles them into a
-   * shader program.
-   *
-   * @param vShaderFile the vertex shader source file
-   * @param gShaderFile the geometry shader source file
-   * @param fShaderFile the fragment shader source file
-   *
-   * @return A handle to the compiled glsl program.
-   */
-  GLuint InitShader( const char* vShaderFile, const char* gShaderFile,
-                     const char* fShaderFile );
+  GLuint InitShader( const char* vShaderFile, const char* fShaderFile, const char* gShaderFile = NULL );
 }
 
 #endif
