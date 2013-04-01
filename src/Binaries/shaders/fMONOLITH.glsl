0c702a9dc126a899dbeb52c995f4a8acb271ba19
nuclearmistake@gmail.com <emccann@cs.uml.edu>
2013-04-01 16:52:22 -0400

    Make the W on the romanticEvening vector romantic too.

--- src/Binaries/shaders/fMONOLITH.glsl
+++ src/Binaries/shaders/fMONOLITH.glsl
@@ -36,7 +36,7 @@ vec4 aRomanticEvening()
     if (lightness < 0.0) lightness = 0.0;
     if (lightness > 1.0) lightness = 1.0;    
   }
-  return lightness * vec4(1.0,1.0,1.0,0.1/lightness);
+  return lightness * vec4(1.0,1.0,1.0,1.0);
 }
 
 void main() 
