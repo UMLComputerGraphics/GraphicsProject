8f76e55f591e591150d228e8969fb293281e5253
nuclearmistake@gmail.com <emccann@cs.uml.edu>
2013-04-15 09:08:05 -0400

    More makefile hacking

--- src/ImageMagick/magick/magick-baseconfig.h
+++ src/ImageMagick/magick/magick-baseconfig.h
@@ -1489,11 +1489,6 @@
 #define MAGICKCORE_ZLIB_DELEGATE 1
 #endif
 
-/* Enable large inode numbers on Mac OS X 10.5.  */
-#ifndef _DARWIN_USE_64_BIT_INODE
-# define _DARWIN_USE_64_BIT_INODE 1
-#endif
-
 /* Number of bits in a file offset, on hosts where this is settable. */
 /* #undef _FILE_OFFSET_BITS */
 
