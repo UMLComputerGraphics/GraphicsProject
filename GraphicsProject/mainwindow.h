935015ca216f47a1ee8b514b693481d05b0efc7e
Franck Kamayou <franckamayou@gmaill.com>
2013-04-15 15:43:39 -0400

    making the QT window blank

--- GraphicsProject/mainwindow.h
+++ GraphicsProject/mainwindow.h
@@ -10,22 +10,11 @@ class MainWindow;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
+    
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-
-public slots:
-    void printTest(int num);
-    void printTest2(int num);
-    void printTest3(int num);
-    void printTest4(int num);
-    void printTest5(int num);
-    void printTest6(int num);
-    void checkChanged(bool b);
-    void checkChanged2(bool b);
-    void checkChanged3(bool b);
-
+    
 private:
     Ui::MainWindow *ui;
 };
