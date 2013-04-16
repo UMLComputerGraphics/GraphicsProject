56d409342262c8364cb3ffc79005150c8f840124
Zachary Maybury <Zachary_Maybury@student.uml.edu>
2013-04-15 23:57:18 -0400

    Subdivision of models into parts to help matching, looking better on original and medium quality models, but still blows up on high quality. Can be improved with modeling with ticket I added earlier today. Also added division of "problem" triangles, but I need to play with this more to determine the best way to decide which triangles to divide. I still need to spend some more time on the quadtree mapping code (currently not being used). More to come, and I also promise I will document all of the code, I just haven't gotten to it, but I will. I will address the quadtree mapping next, as well as I need to come up with a better way of breaking matching ties, which occur extremely frequently (and usually there are many many matches for the same triangle) in the high quality models.
    I hope to also look over the shader files to see if I can get in there some more interesting movement functions as well as the ability to take in parametric equations for the morphing.

--- src/include/modelFunctions.hpp
+++ src/include/modelFunctions.hpp
@@ -21,13 +21,25 @@ using Angel::vec3;
 using Angel::vec4;
 
 void makeModelsSameSize( Object* model1, Object* model2 );
+void splitProblemTriangles(Object* model1, Object* model2);
+float getTriangleSize(vec4 a, vec4 b, vec4 c);
+void segmentModels(Object* model1, vec3 model1Low, vec3 model1High, Object* model2, vec3 model2Low, vec3 model2High);
 int findTriangleWithMinimumDistance( Object* largerModel, Object* smallerModel,
                                      int index );
+void divideTriangle(Object* model, int index);
+void makeModelTopSameSize(std::vector< Angel::vec4 >& model1Vertices,std::vector< Angel::vec3 >& model1Normals,std::vector< Angel::vec4 >& model1Colors,std::vector< Angel::vec2 >& model1Textures, std::vector< Angel::vec4 >& model2Vertices,std::vector< Angel::vec3 >& model2Normals,std::vector< Angel::vec4 >& model2Colors,std::vector< Angel::vec2 >& model2Textures);
 float threeDimensionalDistance( vec4 points1, vec4 points2 );
+float threeDimensionalDistanceWithOrigin( vec4 points1, vec4 points2);
 void matchInitialPoints(Object* model1, Object* model2);
+void matchPoints(std::vector< Angel::vec4 >& model1Vertices,std::vector< Angel::vec3 >& model1Normals,std::vector< Angel::vec4 >& model1Colors,std::vector< Angel::vec2 >& model1Textures, std::vector< Angel::vec4 >& model2Vertices,std::vector< Angel::vec3 >& model2Normals,std::vector< Angel::vec4 >& model2Colors,std::vector< Angel::vec2 >& model2Textures);
 float threeDimensionalDistanceWithNormals(vec4 points1, vec3 normals1, vec4 points2, vec3 normals2);
 float threeDimensionalDistanceFromCenter(vec4 points1, vec4 points2);
 vec4 findCenter(vec4 point1, vec4 point2, vec4 point3);
+void findOptimalOrientation(vec4 a, vec4 b, vec4 c, Object* model, int index, vec4& point1, vec4& point2, vec4& point3);
+void findOptimalOrientation(vec4 a, vec4 b, vec4 c, std::vector< Angel::vec4 > model, int index, vec4& point1, vec4& point2, vec4& point3);
+void findOptimalOrientation(vec4 a, vec4 b, vec4 c, Object* model, int index, int& point1, int& point2, int& point3);
 int findTriangleWithMinimumDistanceFromCenter(Object* largerModel, Object* smallerModel, int index);
+int findTriangleWithMinimumDistanceFromCenter(std::vector< Angel::vec4 > largerPoints, std::vector< Angel::vec4 > smallerPoints, int index);
+void applyToObjects(Object* model1, Object* model2, std::vector< Angel::vec4 > model1Vertices[3],std::vector< Angel::vec3 > model1Normals[3],std::vector< Angel::vec4 > model1Colors[3],std::vector< Angel::vec2 > model1Textures[3],std::vector< Angel::vec4 > model2Vertices[3],std::vector< Angel::vec3 > model2Normals[3],std::vector< Angel::vec4 > model2Colors[3],std::vector< Angel::vec2 > model2Textures[3]);
 
 #endif
