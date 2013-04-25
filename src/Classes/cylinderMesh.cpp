8ded24f2fb89d7fcc620e99ca665f2195d1c2713
jnsnow <jhuston@cs.uml.edu>
2013-04-25 11:22:03 -0400

    Fixing undefined 'pi' variable for linux

--- src/Classes/cylinderMesh.cpp
+++ src/Classes/cylinderMesh.cpp
@@ -6,6 +6,7 @@
  * @details FIXME: Documentation from Zach
  */
 
+#include "globals.h" // for pi
 #include "cylinderMesh.hpp"
 #include "Object.hpp"
 #include <stdexcept>
@@ -20,8 +21,8 @@ std::vector< Angel::vec4 > createCylinder(float height, float width){
 	int slices=10;
 	int halfLength=height/2.0;
 	for(int i=0; i<slices; i++) {
-		float theta = ((float)i)*2.0*pi;
-		float nextTheta = ((float)i+1)*2.0*pi;
+		float theta = ((float)i)*2.0*M_PI;
+		float nextTheta = ((float)i+1)*2.0*M_PI;
 		vertices.push_back(vec4(0.0,halfLength,0.0,0.0));
 		vertices.push_back(vec4(radius*cos(theta), halfLength, radius*sin(theta)));
 		vertices.push_back(vec4(radius*cos(nextTheta), halfLength, radius*sin(nextTheta)));
