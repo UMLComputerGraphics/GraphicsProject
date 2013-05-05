87e1b2debfbeb01ef0e62f77ae88afd8dca83284
nuclearmistake@gmail.com <emccann@cs.uml.edu>
2013-05-05 16:11:25 -0400

    bandaid for qt because I need to work, despite the files for this function unrelated to what I'm testing not existing

--- src/Classes/MONOLITH.cpp
+++ src/Classes/MONOLITH.cpp
@@ -178,8 +178,10 @@ void MONOLITH::slotParticleFieldFunction(int index)
 
 void MONOLITH::slotUpdateVectorField(std::string* params)
 {
+#ifdef UserParameters
     Parameters* funcParams = new UserParameters(params);
     ps->setFuncParams(funcParams);
+#endif
 }
 
 /**
