144f85dff2b1cc3b3ce5463443f5b72b0e577de1
jnsnow <jhuston@cs.uml.edu>
2013-05-08 13:14:37 -0400

    Adding HSV rainbow color function

--- src/Classes/ColorFunctions.cpp
+++ src/Classes/ColorFunctions.cpp
@@ -70,6 +70,34 @@ float scaleOverRange( float min, float max, float pct ) {
   return pct / (max - min);
 }
 
+vec4 ColorFunctions::HSV( float lifePct, vec4 posIn ) {
+  
+  float hue = lifePct;
+
+  static const float S = 1.0;
+  static const float V = 1.0;
+  static const float M = 0.0;
+  float H, F, N, I;
+  int i;
+  float &K = F;
+
+  H = (hue * 6.0);
+  I = floor( H );
+  F = (H - I);
+  N = (1.0 - F);
+  
+  i = (int)I;
+  
+  if (i == 0) return vec4( V, K, M, 1 );
+  if (i == 1) return vec4( N, V, M, 1 );
+  if (i == 2) return vec4( M, V, K, 1 );
+  if (i == 3) return vec4( M, N, V, 1 );
+  if (i == 4) return vec4( K, M, V, 1 );
+
+  return vec4( V, M, N, 1 );
+  
+}
+
 vec4 ColorFunctions::rainbow( float lifePct, vec4 posIn ) {
   float r, g, b, w;
 
